12b5a7aeb14394135e2c054c64fd79550def6920
ofer.karp <ofer.karp@gmail.com>
2021-09-16 18:21:39 +0300

    Add model items

---
+++ src/main/java/org/ok/starfish/data/controller/Paths.java
@@ -0,0 +1,10 @@
+package org.ok.starfish.data.controller;
+
+public interface Paths {
+
+    String BASE_PATH = "api/rest";
+
+    String COUNT_PATH = "/count";
+
+    String APPLICATION_CATEGORY_PATH = BASE_PATH + "/applicationCategory";
+}
--- src/main/java/org/ok/starfish/data/controller/application/ApplicationCategoryController.java
+++ src/main/java/org/ok/starfish/data/controller/application/ApplicationCategoryController.java
@@ -4,11 +4,15 @@ import org.ok.starfish.data.service.application.ApplicationCategoryService;
 import org.ok.starfish.model.application.ApplicationCategory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static org.ok.starfish.data.controller.Paths.APPLICATION_CATEGORY_PATH;
+
 @RestController
+@RequestMapping(path = APPLICATION_CATEGORY_PATH)
 public class ApplicationCategoryController {
 
     private final ApplicationCategoryService applicationCategoryService;
