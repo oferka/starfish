5f39249c3428f0ead4c3939119efee87d2adaf63
ofer.karp <ofer.karp@gmail.com>
2021-09-16 23:46:06 +0300

    Add model items

--- src/main/java/org/ok/starfish/data/controller/application/ApplicationCategoryController.java
+++ src/main/java/org/ok/starfish/data/controller/application/ApplicationCategoryController.java
@@ -1,5 +1,11 @@
 package org.ok.starfish.data.controller.application;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.ok.starfish.data.service.application.ApplicationCategoryService;
 import org.ok.starfish.model.application.ApplicationCategory;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +28,11 @@ public class ApplicationCategoryController {
     }
 
     @GetMapping
+    @Operation(summary = "Find all application categories")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Application categories successfully found", content = { @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ApplicationCategory.class)))}),
+            @ApiResponse(responseCode = "400", description = "Failed to find application categories", content = @Content) }
+    )
     public ResponseEntity<List<ApplicationCategory>> findAll() {
         List<ApplicationCategory> allItems = applicationCategoryService.findAll();
         return ResponseEntity.ok(allItems);
