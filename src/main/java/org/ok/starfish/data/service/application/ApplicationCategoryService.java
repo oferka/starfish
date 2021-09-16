833c47abdd14af299b7121377b28c94b49e64c09
ofer.karp <ofer.karp@gmail.com>
2021-09-16 17:30:32 +0300

    Add model items

---
+++ src/main/java/org/ok/starfish/data/service/application/ApplicationCategoryService.java
@@ -0,0 +1,23 @@
+package org.ok.starfish.data.service.application;
+
+import org.ok.starfish.model.application.ApplicationCategory;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ApplicationCategoryService {
+
+    List<ApplicationCategory> findAll();
+
+    Optional<ApplicationCategory> findById(String id);
+
+    long count();
+
+    ApplicationCategory save(ApplicationCategory applicationCategory);
+
+    Iterable<ApplicationCategory> save(Iterable<ApplicationCategory> applicationCategories);
+
+    void deleteById(String id);
+
+    boolean existsById(String id);
+}
---
+++ src/main/java/org/ok/starfish/data/service/application/ApplicationCategoryServiceElasticsearch.java
@@ -0,0 +1,58 @@
+package org.ok.starfish.data.service.application;
+
+import org.ok.starfish.data.repository.es.application.ApplicationCategoryElasticsearchRepository;
+import org.ok.starfish.model.application.ApplicationCategory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class ApplicationCategoryServiceElasticsearch implements ApplicationCategoryService {
+
+    private final ApplicationCategoryElasticsearchRepository applicationCategoryElasticsearchRepository;
+
+    public ApplicationCategoryServiceElasticsearch(ApplicationCategoryElasticsearchRepository applicationCategoryElasticsearchRepository) {
+        this.applicationCategoryElasticsearchRepository = applicationCategoryElasticsearchRepository;
+    }
+
+    @Override
+    public List<ApplicationCategory> findAll() {
+        Iterable<ApplicationCategory> applicationCategories = applicationCategoryElasticsearchRepository.findAll();
+        return StreamSupport
+                .stream(applicationCategories.spliterator(), false)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<ApplicationCategory> findById(String id) {
+        return applicationCategoryElasticsearchRepository.findById(id);
+    }
+
+    @Override
+    public ApplicationCategory save(ApplicationCategory applicationCategory) {
+        return applicationCategoryElasticsearchRepository.save(applicationCategory);
+    }
+
+    @Override
+    public Iterable<ApplicationCategory> save(Iterable<ApplicationCategory> applicationCategories) {
+        return applicationCategoryElasticsearchRepository.saveAll(applicationCategories);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        applicationCategoryElasticsearchRepository.deleteById(id);
+    }
+
+    @Override
+    public boolean existsById(String id) {
+        return applicationCategoryElasticsearchRepository.existsById(id);
+    }
+
+    @Override
+    public long count() {
+        return applicationCategoryElasticsearchRepository.count();
+    }
+}
