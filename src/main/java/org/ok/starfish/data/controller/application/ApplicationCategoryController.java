c3211d0b94f357099470158121a97e21f6d58947
ofer.karp <ofer.karp@gmail.com>
2021-09-17 15:39:31 +0300

    Add model items

--- src/main/java/org/ok/starfish/data/controller/application/ApplicationCategoryController.java
+++ src/main/java/org/ok/starfish/data/controller/application/ApplicationCategoryController.java
@@ -9,16 +9,19 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.ok.starfish.data.service.application.ApplicationCategoryService;
 import org.ok.starfish.model.application.ApplicationCategory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import static org.ok.starfish.data.controller.Paths.APPLICATION_CATEGORY_PATH;
+import static org.ok.starfish.data.controller.Paths.COUNT_PATH;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
 @RequestMapping(path = APPLICATION_CATEGORY_PATH)
@@ -50,5 +53,53 @@ public class ApplicationCategoryController {
     public ResponseEntity<ApplicationCategory> findById(@Parameter(description = "The id of the application category to be found") @PathVariable("id") String id) {
         Optional<ApplicationCategory> applicationCategory = applicationCategoryService.findById(id);
         return applicationCategory.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @Operation(summary = "Create an application category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Application category created successfully", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationCategory.class))}),
+            @ApiResponse(responseCode = "400", description = "Failed to create an application category", content = @Content) })
+    @PostMapping
+    public ResponseEntity<ApplicationCategory> save(@Parameter(description = "Application category to be saved") @RequestBody @Valid ApplicationCategory applicationCategory) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        URI location = linkTo(ApplicationCategoryController.class).slash(applicationCategory.getId()).toUri();
+        httpHeaders.setLocation(location);
+        ApplicationCategory saved = applicationCategoryService.save(applicationCategory);
+        return new ResponseEntity<>(saved, httpHeaders, HttpStatus.CREATED);
+    }
+
+    @Operation(summary = "Delete an application category by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Application category successfully deleted by id"),
+            @ApiResponse(responseCode = "400", description = "Failed to delete application category by id", content = @Content) })
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@Parameter(description = "The id of the application category to be deleted") @PathVariable("id") String id) {
+        applicationCategoryService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "Update an application category by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Application category updated successfully", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationCategory.class))}),
+            @ApiResponse(responseCode = "404", description = "Application category with specified id was not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Failed to update an application category with specified id", content = @Content) })
+    @PutMapping("/{id}")
+    public ResponseEntity<ApplicationCategory> update(@Parameter(description = "The id of the application category to be updated") @PathVariable("id") String id, @Parameter(description = "Application category to be updated") @RequestBody ApplicationCategory applicationCategory) {
+        boolean isPresent = applicationCategoryService.existsById(id);
+        if (!isPresent) {
+            return ResponseEntity.notFound().build();
+        }
+        ApplicationCategory updated = applicationCategoryService.save(applicationCategory);
+        return ResponseEntity.ok(updated);
+    }
+
+    @Operation(summary = "Return the number of existing application categories")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Application category counted successfully", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Long.class))}),
+            @ApiResponse(responseCode = "400", description = "Failed to count application categories", content = @Content) })
+    @GetMapping(path = COUNT_PATH)
+    public ResponseEntity<Long> count() {
+        long count = applicationCategoryService.count();
+        return ResponseEntity.ok(count);
     }
 }
