b0520b86c1916831ee19e887785ddd32e79c4aa2
ofer.karp <ofer.karp@gmail.com>
2021-09-19 19:32:49 +0300

    Add model items

--- src/test/java/org/ok/starfish/data/controller/application/ApplicationCategoryControllerTest.java
+++ src/test/java/org/ok/starfish/data/controller/application/ApplicationCategoryControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Optional;
 
@@ -81,6 +82,17 @@ public class ApplicationCategoryControllerTest {
     }
 
     @Test
+    public void shouldNotFindById() throws Exception {
+        MvcResult mvcResult = mvc.perform(get(format("/%s/{id}", APPLICATION_CATEGORY_PATH), getNonExistingId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(log())
+                .andExpect(status().isNotFound())
+                .andReturn();
+        assertNotNull(mvcResult);
+    }
+
+    @Test
     public void shouldSave() throws Exception {
         ApplicationCategory item = sampleApplicationCategoryProvider.getItem();
         MvcResult mvcResult = mvc.perform(post(format("/%s", APPLICATION_CATEGORY_PATH))
@@ -112,6 +124,17 @@ public class ApplicationCategoryControllerTest {
     }
 
     @Test
+    public void shouldNotDeleteById() throws Exception {
+        MvcResult mvcResult = mvc.perform(delete(format("/%s/{id}", APPLICATION_CATEGORY_PATH), getNonExistingId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(log())
+                .andExpect(status().isNoContent())
+                .andReturn();
+        assertNotNull(mvcResult);
+    }
+
+    @Test
     public void shouldUpdate() throws Exception {
         ApplicationCategory item = sampleApplicationCategoryProvider.getItem();
         ApplicationCategory saved = applicationCategoryElasticsearchRepository.save(item);
@@ -130,6 +153,21 @@ public class ApplicationCategoryControllerTest {
     }
 
     @Test
+    public void shouldNotUpdate() throws Exception {
+        ApplicationCategory item = sampleApplicationCategoryProvider.getItem();
+        ApplicationCategory saved = applicationCategoryElasticsearchRepository.save(item);
+        MvcResult mvcResult = mvc.perform(put(format("/%s/{id}", APPLICATION_CATEGORY_PATH), getNonExistingId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(item))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(log())
+                .andExpect(status().isNotFound())
+                .andReturn();
+        assertNotNull(mvcResult);
+        applicationCategoryElasticsearchRepository.deleteById(saved.getId());
+    }
+
+    @Test
     public void shouldCount() throws Exception {
         List<ApplicationCategory> items = sampleApplicationCategoryProvider.getItems(numberOfItems);
         Iterable<ApplicationCategory> saved = applicationCategoryElasticsearchRepository.saveAll(items);
@@ -141,5 +179,9 @@ public class ApplicationCategoryControllerTest {
                 .andReturn();
         assertNotNull(mvcResult);
         applicationCategoryElasticsearchRepository.deleteAll(saved);
+    }
+
+    private @NotNull String getNonExistingId() {
+        return "No Such ID";
     }
 }
