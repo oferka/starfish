ff272558817073dbab9daaee962c60725bc5ba92
ofer.karp <ofer.karp@gmail.com>
2021-09-26 18:16:13 +0300

    Add model items

---
+++ src/main/java/org/ok/starfish/data/content/health/user/DeviceContentHealthIndicator.java
@@ -0,0 +1,32 @@
+package org.ok.starfish.data.content.health.user;
+
+import org.ok.starfish.data.service.user.DeviceService;
+import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component("deviceContent")
+@ConditionalOnEnabledHealthIndicator("deviceContent")
+public class DeviceContentHealthIndicator implements HealthIndicator {
+
+    private final DeviceService deviceService;
+
+    public DeviceContentHealthIndicator(DeviceService deviceService) {
+        this.deviceService = deviceService;
+    }
+
+    @Override
+    public Health health() {
+        Health.Builder status;
+        try {
+            long count = deviceService.count();
+            status = (count > 0)?Health.up():Health.down();
+            status.withDetail("number_of_devices", count);
+        }
+        catch (Exception e) {
+            status = Health.down(e);
+        }
+        return status.build();
+    }
+}
