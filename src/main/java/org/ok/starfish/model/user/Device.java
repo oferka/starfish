2990ba51b91049686c8a9db73197c8f7012c6f83
ofer.karp <ofer.karp@gmail.com>
2021-09-26 17:14:57 +0300

    Add model items

--- src/main/java/org/ok/starfish/model/user/Device.java
+++ src/main/java/org/ok/starfish/model/user/Device.java
@@ -1,4 +1,41 @@
 package org.ok.starfish.model.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import static org.springframework.data.elasticsearch.annotations.FieldType.Keyword;
+
+@ToString
+@AllArgsConstructor
+@Document(indexName = "starfish_device")
 public class Device {
+
+    @Id
+    @Getter
+    @NotNull
+    @Size(min = 2, max = 64)
+    @NotBlank
+    @Field(type = Keyword)
+    private final String id;
+
+    @Getter
+    @NotNull
+    @Size(min = 2, max = 64)
+    @NotBlank
+    @Field(type = Keyword)
+    private final String name;
+
+    @Getter
+    @NotNull
+    @Field(type = FieldType.Object)
+    private User user;
 }
