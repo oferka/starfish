9203d0d19b0ab440a681138a0125c5df163ab3de
ofer.karp <ofer.karp@gmail.com>
2021-09-26 17:34:25 +0300

    Add model items

---
+++ src/main/java/org/ok/starfish/data/service/user/DeviceService.java
@@ -0,0 +1,32 @@
+package org.ok.starfish.data.service.user;
+
+import org.ok.starfish.model.user.Device;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
+
+public interface DeviceService {
+
+    @NotNull List<Device> findAll();
+
+    @NotNull Optional<Device> findById(@NotNull String id);
+
+    @NotNull Optional<Device> findByName(@NotNull String name);
+
+    @NotNull Optional<Device> findRandom();
+
+    long count();
+
+    @NotNull Device save(@NotNull Device device);
+
+    @NotNull Iterable<Device> saveAll(@NotNull Iterable<Device> devices);
+
+    @NotNull Optional<Device> update(@NotNull String id, @NotNull Device device);
+
+    void deleteById(@NotNull String id);
+
+    boolean existsById(@NotNull String id);
+
+    boolean exists(@NotNull @NotNull Device device);
+}
---
+++ src/main/java/org/ok/starfish/data/service/user/DeviceServiceImpl.java
@@ -0,0 +1,90 @@
+package org.ok.starfish.data.service.user;
+
+import org.apache.commons.lang3.RandomUtils;
+import org.ok.starfish.data.repository.es.user.DeviceElasticsearchRepository;
+import org.ok.starfish.model.user.Device;
+import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class DeviceServiceImpl implements DeviceService {
+
+    private final DeviceElasticsearchRepository deviceElasticsearchRepository;
+
+    public DeviceServiceImpl(DeviceElasticsearchRepository deviceElasticsearchRepository) {
+        this.deviceElasticsearchRepository = deviceElasticsearchRepository;
+    }
+
+    @Override
+    public @NotNull List<Device> findAll() {
+        Iterable<Device> items = deviceElasticsearchRepository.findAll();
+        return StreamSupport
+                .stream(items.spliterator(), false)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public @NotNull Optional<Device> findById(@NotNull String id) {
+        return deviceElasticsearchRepository.findById(id);
+    }
+
+    @Override
+    public @NotNull Optional<Device> findByName(@NotNull String name) {
+        return deviceElasticsearchRepository.findByName(name);
+    }
+
+    @Override
+    public Optional<Device> findRandom() {
+        List<Device> items = findAll();
+        if(items.isEmpty()) {
+            return Optional.empty();
+        }
+        Device item = items.get(RandomUtils.nextInt(0, items.size()));
+        return Optional.of(item);
+    }
+
+    @Override
+    public @NotNull Device save(@NotNull Device device) {
+        return deviceElasticsearchRepository.save(device);
+    }
+
+    @Override
+    public @NotNull Iterable<Device> saveAll(@NotNull Iterable<Device> devices) {
+        return deviceElasticsearchRepository.saveAll(devices);
+    }
+
+    @Override
+    public Optional<Device> update(@NotNull String id, @NotNull Device device) {
+        Optional<Device> result = Optional.empty();
+        if(existsById(id)) {
+            result = Optional.of(save(device));
+        }
+        return result;
+    }
+
+    @Override
+    public void deleteById(@NotNull String id) {
+        deviceElasticsearchRepository.deleteById(id);
+    }
+
+    @Override
+    public long count() {
+        return deviceElasticsearchRepository.count();
+    }
+
+    @Override
+    public boolean existsById(@NotNull String id) {
+        return deviceElasticsearchRepository.existsById(id);
+    }
+
+    @Override
+    public boolean exists(@NotNull Device device) {
+        Optional<Device> optionalDevice = deviceElasticsearchRepository.findByName(device.getName());
+        return optionalDevice.isPresent();
+    }
+}
