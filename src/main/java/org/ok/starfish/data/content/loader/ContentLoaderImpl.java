cc2d964099ec64438c4bfb9a7a82024d80191500
ofer.karp <ofer.karp@gmail.com>
2021-09-26 09:14:42 +0300

    Add model items

--- src/main/java/org/ok/starfish/data/content/loader/ContentLoaderImpl.java
+++ src/main/java/org/ok/starfish/data/content/loader/ContentLoaderImpl.java
@@ -1,8 +1,11 @@
 package org.ok.starfish.data.content.loader;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.IterableUtils;
+import org.ok.starfish.data.content.loader.account.AccountContentLoader;
 import org.ok.starfish.data.content.loader.application.ApplicationCategoryContentLoader;
 import org.ok.starfish.data.content.loader.application.ApplicationContentLoader;
+import org.ok.starfish.model.account.Account;
 import org.ok.starfish.model.application.Application;
 import org.ok.starfish.model.application.ApplicationCategory;
 import org.springframework.stereotype.Service;
@@ -13,15 +16,19 @@ public class ContentLoaderImpl implements ContentLoader {
 
     private final ApplicationCategoryContentLoader applicationCategoryContentLoader;
     private final ApplicationContentLoader applicationContentLoader;
+    private final AccountContentLoader accountContentLoader;
 
-    public ContentLoaderImpl(ApplicationCategoryContentLoader applicationCategoryContentLoader, ApplicationContentLoader applicationContentLoader) {
+    public ContentLoaderImpl(ApplicationCategoryContentLoader applicationCategoryContentLoader, ApplicationContentLoader applicationContentLoader, AccountContentLoader accountContentLoader) {
         this.applicationCategoryContentLoader = applicationCategoryContentLoader;
         this.applicationContentLoader = applicationContentLoader;
+        this.accountContentLoader = accountContentLoader;
     }
 
     @Override
     public void ensureContentLoaded() {
         Iterable<ApplicationCategory> applicationCategories = applicationCategoryContentLoader.ensureContentLoaded();
         Iterable<Application> applications = applicationContentLoader.ensureContentLoaded();
+        Iterable<Account> accounts = accountContentLoader.ensureContentLoaded();
+        log.info("{} content entities ensured loaded", IterableUtils.size(applicationCategories) + IterableUtils.size(applications) + IterableUtils.size(accounts));
     }
 }
--- src/main/java/org/ok/starfish/data/content/provider/account/AccountContentProviderImpl.java
+++ src/main/java/org/ok/starfish/data/content/provider/account/AccountContentProviderImpl.java
@@ -17,16 +17,16 @@ public class AccountContentProviderImpl implements AccountContentProvider {
     @Override
     public List<Account> get() {
         List<Account> result =  asList(
-                getAccount("Application Category 1"),
-                getAccount("Application Category 2"),
-                getAccount("Application Category 3"),
-                getAccount("Application Category 4"),
-                getAccount("Application Category 5"),
-                getAccount("Application Category 6"),
-                getAccount("Application Category 7"),
-                getAccount("Application Category 8"),
-                getAccount("Application Category 9"),
-                getAccount("Application Category 10")
+                getAccount("Account 1"),
+                getAccount("Account 2"),
+                getAccount("Account 3"),
+                getAccount("Account 4"),
+                getAccount("Account 5"),
+                getAccount("Account 6"),
+                getAccount("Account 7"),
+                getAccount("Account 8"),
+                getAccount("Account 9"),
+                getAccount("Account  10")
         );
         log.info("{} accounts provided", result.size());
         return result;
